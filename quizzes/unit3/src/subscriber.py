69b61ea321eecab0e4d08cfcd758904fc281a8d6
Rojas Rafael <rafael.rojas@unibz.it>
2020-08-04 20:05:10 +0200

    adding stuff

---
+++ quizzes/unit3/src/subscriber.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+def main():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('main', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
