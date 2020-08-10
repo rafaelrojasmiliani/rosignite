12c538e0e011954dc68a711a726705034d11f040
Rojas Rafael <rafael.rojas@unibz.it>
2020-08-10 18:49:36 +0200

    wip

--- unit04/src/unit_4_services/src/bb8_move_custom_service_client.py
+++ unit04/src/unit_4_services/src/bb8_move_custom_service_client.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 import rospy
-from my_custom_srv_msg_pkg.srv import MyCustomServiceMessage, MyCustomServiceMessageResponse # you import the service message python classes 
+from my_custom_srv_msg_pkg.srv import MyCustomServiceMessage, MyCustomServiceMessageRequest # you import the service message python classes 
 
 def main():
     rospy.init_node('exercise_4_3_client')
-    rospy.wait_for_service('/move_bb8_in_circle')
-    service = rospy.ServiceProxy('/move_bb8_in_circle', Empty)
-    service()
+    rospy.wait_for_service('/move_bb8_in_circle_custom')
+    service = rospy.ServiceProxy('/move_bb8_in_circle_custom', MyCustomServiceMessage)
+    req = MyCustomServiceMessageRequest()
+    req.duration = 5
+    service(req)
 
 if __name__ == '__main__':
     main()
