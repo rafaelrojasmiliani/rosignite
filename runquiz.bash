64ab5db5d749fbecd303b99a5989b13c109a6525
Rojas Rafael <rafael.rojas@unibz.it>
2020-08-10 10:23:42 +0200

    wip

--- runquiz.bash
+++ runquiz.bash
@@ -34,7 +34,7 @@ function build_package(){
 }
 
 function is_built(){
-     [ -d "quizzes/$1/devel" ] && [ -d "quizzes/$1/build"]
+     [[ -d "quizzes/$1/devel" &&  -d "quizzes/$1/build"]]
      return $?
 }
 
