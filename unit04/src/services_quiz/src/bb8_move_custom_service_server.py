20fc618a8b8f4ac92b32b3006534de05915d11da
Rojas Rafael <rafael.rojas@unibz.it>
2020-08-11 09:26:31 +0200

    wip

--- unit04/src/services_quiz/src/bb8_move_custom_service_server.py
+++ unit04/src/services_quiz/src/bb8_move_custom_service_server.py
@@ -8,13 +8,13 @@ class cServiceServer(object):
     def __init__(self):
         self.srv_ = rospy.Service('/move_bb8_in_square_custom', BB8CustomServiceMessage , self.service) 
         self.pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        self.time_step_ = 1
-        self.rate_ = rospy.Rate(self.time_step_)
+        self.time_step_ = 0.1
+        self.rate_ = rospy.Rate(1.0/self.time_step_)
 
 
     def service(self, *_data):
         request = _data[0]
-        time_size_scale = 0.05
+        time_size_scale = 5
         repetitions = request.repetitions
         side = request.side*time_size_scale
 
@@ -30,29 +30,35 @@ class cServiceServer(object):
     def move_linear(self, _side, _vel=1.0):
         command = Twist()
         command.linear.x = _vel
-        self.pub_.publish(command)
-        self.rate_.sleep()
+        displacement = 0.0
+        while displacement < _side:
+            self.pub_.publish(command)
+            displacement += _vel*self.time_step_
+            rospy.loginfo('move linear {:.4f}'.format(displacement))
+            self.rate_.sleep()
         command.linear.x = 0.0
         self.pub_.publish(command)
         self.rate_.sleep()
-        command.angular.z = _vel
+        command.linear.y = _vel
         displacement = 0.0
-        self.pub_.publish(command)
-        displacement += _vel*self.time_step_
-        self.rate_.sleep()
-        command.angular.z = 0.0
+        while displacement < _side:
+            self.pub_.publish(command)
+            displacement += _vel*self.time_step_
+            rospy.loginfo('move linear {:.4f}'.format(displacement))
+            self.rate_.sleep()
+        command.linear.y = 0.0
         self.pub_.publish(command)
         self.rate_.sleep()
 
     def move_angular(self, _vel=5.0):
         command = Twist()
-#        command.linear.y = _vel
-#        displacement = 0.0
-#        while displacement < np.pi/2.0:
-#            self.pub_.publish(command)
-#            rospy.loginfo('move angular {:.4f}'.format(displacement))
-#            self.rate_.sleep()
-#            displacement += _vel*self.time_step_
+        command.linear.y = _vel
+        displacement = 0.0
+        while displacement < np.pi/2.0:
+            self.pub_.publish(command)
+            rospy.loginfo('move angular {:.4f}'.format(displacement))
+            self.rate_.sleep()
+            displacement += _vel*self.time_step_
 
 
 def main():
