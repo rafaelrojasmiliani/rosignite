2cd9d45e0ef8c479df07afc5ca0538e9fb75e95b
Rojas Rafael <rafael.rojas@unibz.it>
2020-08-12 19:07:04 +0200

    wip

---
+++ project_sphero/src/my_sphero_topics/src/collision_detector.py
@@ -0,0 +1,54 @@
+
+#! /usr/bin/env python
+
+import rospy
+from .node_imu_service import cImuReader
+from std_srvs.srv import Trigger, TriggerResponse
+import numpy as np
+
+
+class cCollisionDetectionService(cImuReader):
+    def __init__(self, _srv_name='/crash_direction_service'):
+        cImuReader.__init__(self, _rate=0.001)
+        self.collition_info_ = None
+        self._my_service_ = rospy.Service(self._srv_name, Trigger , self.srv_callback)
+        self._threshhold = 7.00
+
+    def in_spin(self):
+        imu_data = self.get_data()
+        acc = np.array([getattr(imu_data.linear_acceleration, comp) for comp in 'xyz'])
+        
+        if np.linalg.norm(acc) > self._threshhold:
+            self.collition_info_ = acc
+        
+        return self.convert_to_dict(message)
+
+    def srv_callback(self, request):
+        
+        response = TriggerResponse()
+        if self.collition_info_ is None:
+            response.success = False
+            response.message = 'forwards'
+            return response
+
+        response.success=True
+
+        acc = service.collition_info_ 
+        service.collition_info_ = None
+        index = np.argmax(np.abs(acc))
+
+        val = acc[index]
+        aux_array = [('forwards', 'backwards'), ('left', 'right'), ('up', 'down')]
+
+        if vel > 0:
+            response.message = aux_array[index][0]
+        else:
+            response.message = aux_array[index][1]
+
+
+        return response
+
+if __name__ == '__main__':
+    rospy.init_node('collision_detector')
+    service = cCollisionDetectionService()
+    service.spin()
