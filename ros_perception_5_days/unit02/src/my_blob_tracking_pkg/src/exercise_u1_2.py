4337ba73be3f959cd8cf63894120347281ea15d1
Rojas Rafael <rafael.rojas@unibz.it>
2021-03-02 11:28:37 +0100

    [ros_perception] exervice u1 2

---
+++ ros_perception_5_days/unit02/src/my_blob_tracking_pkg/src/exercise_u1_2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from cmvision.msg import Blobs, Blob
+
+
+class SimpleTracker:
+    def __init__(self):
+        self.xpos_ = (180+220)*0.5
+        self.control_pub_ = rospy.Publisher(
+            '/mira/commands/velocity', Twist, queue_size=1)
+        self.blob_subs_ = rospy.Subscriber('/blobs', Blobs, self.get_blobs)
+
+    def get_blobs(self, _msg):
+        for blob in _msg.blobs:
+            if blob.name == "RedBall":
+                self.xpos_ = blob.x
+                break
+
+    def spin(self):
+        twist = Twist()
+        desired = (180+220)*0.5
+        gain = 0.01
+
+        while not rospy.is_shutdown():
+            # turn if we hit the line
+            twist.linear.x = 0.0
+            twist.linear.y = 0
+            twist.linear.z = 0
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = gain*(desired - self.xpos_)
+            self.control_pub_.publish(twist)
+            rospy.sleep(0.1)
+
+
+def main():
+    rospy.init_node("track_blob_color_node", log_level=rospy.WARN)
+    tracker = SimpleTracker()
+    tracker.spin()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
